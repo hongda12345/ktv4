cf84a0fba74759cb3dedab809ae3525ac88cfa13
hongda <1020864884@qq.com>
2017-11-11 21:25:00 +0800

    'data'

---
+++ App/shop.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 宏达
+ * Date: 2017/11/8
+ * Time: 14:53
+ */
+class shop{
+    function add(){
+        echo 'add';
+    }
+    function delete(){
+        echo 'delete';
+    }
+    function index(){
+
+    }
+}
